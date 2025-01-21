da070cc9c649d51d86c900844b3e5ab4de1af32d
Maxamed459 <maxamedmahdi459@gmail.com>
2025-01-21 21:03:15 +0300

    modified navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'primary': "#3238f2",
+        'secendary': "#6b7280",
+      },
       fontFamily:{
         sans:["poppins"]
       }
